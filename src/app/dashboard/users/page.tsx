d27eec17866e1ad74296c7eb3034201c678d0d09
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-11 03:56:38 +0700

    feat: Add Panel component with entry selection feature

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,24 +1,15 @@
-import {Icon} from "@iconify/react";
+"use client";
+
 import TableNext from "@/app/components/TableNext";
+import Panel from "@/app/components/Panel";
+import Button from "@/app/components/Button";
 
 export default function Page () {
     return (
         <main className="w-full">
-            <div className="bg-[#CCDCF3] px-8 py-4 mx-2 rounded-2xl">
-                <button className="bg-primary flex items-center px-3 py-2 rounded-md text-white tracking-wide hover:bg-[#607EAA]">
-                    <Icon icon="mingcute:add-fill" />
-                    Tambah User
-                </button>
-                <div className="flex mt-4 gap-3 items-center text-text-dark">
-                    <p>Show</p>
-                    <select name="entries" id="endtries" className="py-2 px-3 rounded-lg">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                    </select>
-                    <p>entries</p>
-                </div>
-            </div>
+            <Panel>
+                <Button onClick={() => alert('data ditambahkan')}>Tambah User</Button>
+            </Panel>
             <TableNext />
         </main>
     )
