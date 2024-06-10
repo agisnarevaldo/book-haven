783db1ae6e9bcd82f69bd73acd46aa894e5a6f01
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-11 03:19:20 +0700

    input login with an icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,42 @@
+"use client";
+
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { Input } from "@nextui-org/react";
 import Image from "next/image";
+import { useState } from "react";
 
 const LoginPage = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => setIsVisible(!isVisible);
     return (
-        <div className="rounded-3xl bg-text text-[#424642] flex p-12 gap-8">
+        <div className="rounded-3xl bg-text text-[#424642] flex p-12 gap-8 shadow-3xl">
             <div className="w-[400px] flex flex-col justify-center gap-8">
                 <Image src="/logo.svg" width={100} height={100} alt="Logo" className="mx-auto"/>
                 <h1 className="font-bold text-2xl text-center">Sistem Informasi Perpustakaan SMPN 1 Cikoneng</h1>
-                <form className="flex flex-col gap-8">
-                    <input type="text" name="username" placeholder="Username"
-                           className="p-2 border border-[#424642] rounded-2xl outline-0"
+                <form className="flex flex-col gap-8 px-10">
+                    <Input 
+                        type="text" 
+                        name="username"
+                        variant="bordered"
+                        placeholder="Username" 
+                        startContent={
+                            <Icon className="text-2xl" icon="majesticons:user-line"/>
+                        }
                     />
-                    <input type="password" name="password" placeholder="Password"
-                           className="p-2 border border-[#424642] rounded-2xl outline-0"
+                    <Input
+                        variant="bordered"
+                        placeholder="Enter your password"
+                        startContent={
+                            <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+                                {isVisible ? (
+                                    <Icon className="text-2xl text-default-400 pointer-events-none" icon="mdi:eye"/>
+                                ) : (
+                                    <Icon className="text-2xl text-default-400 pointer-events-none" icon="mdi:eye-off"/>
+                                )}
+                            </button>
+                        }
+                        type={isVisible ? "text" : "password"}
                     />
                     <button className="bg-[#1c3879] rounded-2xl text-white py-2" type="submit">Login</button>
                 </form>
