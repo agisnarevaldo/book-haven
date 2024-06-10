8c1a895e0c20f40ea853a7a4d1662332663963c0
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-11 03:22:34 +0700

    feat: Add "Tambah Denda" button to dashboard/denda page

---
+++ src/app/dashboard/denda/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import Panel from "@/app/components/Panel";
+
+export default function Page() {
+    return (
+        <main className="w-full">
+            <Panel>
+                <Button onClick={() => alert('data ditambahkan')}>Tambah Denda</Button>
+            </Panel>
+        </main>
+    )
+}
