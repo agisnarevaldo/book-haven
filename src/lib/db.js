5a08bcab86db274193f3bf6a480c6409ca08a231
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-06-20 22:03:56 +0700

    create db pool

---
+++ src/lib/db.js
@@ -0,0 +1,17 @@
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+export const query = (query, values) => new Promise((resolve, reject) => {
+    pool.execute(query, values, (error, results) => {
+        if (error) return reject(error);
+        resolve(results);
+    });
+});
+
+export default pool;
